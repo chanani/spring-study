1f79c50b0a2c5ca56ffd668b98ec365b370ad9ea
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-01 18:42:04 +0900

    [JPA] 상속 관계 매핑 예제에 BaseEntity 추가

--- jpashop/src/main/java/japbook/japshopp/domain/Delivery.java
+++ jpashop/src/main/java/japbook/japshopp/domain/Delivery.java
@@ -3,7 +3,7 @@ package japbook.japshopp.domain;
 import javax.persistence.*;
 
 @Entity
-public class Delivery {
+public class Delivery extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
