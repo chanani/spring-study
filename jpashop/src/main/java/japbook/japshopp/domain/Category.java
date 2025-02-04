e1609325841e73fad1eb5ea0c38d7e08dbc5e1f2
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-04 20:48:35 +0900

    [JPA] 영속성 전의 설정

--- jpashop/src/main/java/japbook/japshopp/domain/Category.java
+++ jpashop/src/main/java/japbook/japshopp/domain/Category.java
@@ -13,7 +13,7 @@ public class Category extends BaseEntity{
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "PATENT_ID")
     private Category parent;
 
