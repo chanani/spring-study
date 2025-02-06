47515a921550d4eb3c17b5531c97eeb9dfec1e5e
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-06 22:08:06 +0900

    [JPA] JPQL 조인

--- jpql/src/main/java/jpql/JpqlMain.java
+++ jpql/src/main/java/jpql/JpqlMain.java
@@ -12,13 +12,21 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            for (int i = 0; i < 50; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
 
             /*Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
                     .setParameter("username", "member1")
@@ -39,13 +47,18 @@ public class JpqlMain {
                     .getResultList(); */
 
             /////////// 페이징
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+            /*List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
                     .setFirstResult(1)
                     .setMaxResults(10)
                     .getResultList();
             for (Member member1 : result) {
                 System.out.println("member1.toString() = " + member1.toString());
-            }
+            } */
+
+            /////////// 조인
+            String query = "select m from Member m left join m.team t on t.name = 'A'";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
