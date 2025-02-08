1ad7d318f2acc91e959b52a3d7bd0d976c8a0fa2
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-08 15:05:39 +0900

    [JPA] JPQL 페치조인이란

--- jpql/src/main/java/jpql/JpqlMain.java
+++ jpql/src/main/java/jpql/JpqlMain.java
@@ -12,19 +12,29 @@ public class JpqlMain {
         tx.begin();
 
         try {
+            Team teamA= new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamB= new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
-            member.setTeam(team);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
 
-            em.persist(member);
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
 
             em.flush();
             em.clear();
@@ -112,15 +122,44 @@ public class JpqlMain {
             }*/
 
             //////////// 경로 표현식
-            String query = "select m.team from Member m";
+            /* String query = "select m.team from Member m";
             List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
             for (Team s : result) {
                 System.out.println("s = " + s);
+            } */
+
+            //////////// 페치 조인
+            // 일반 쿼리
+            /*String query = "select m from Member m";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+                // 모든 다른 팀에 회원이 100명 있을 경우 -> N + 1
+            } */
+
+            // 1 : N 페치 조인
+            /* String query = "select m from Member m join fetch m.team";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+            } */
+
+            // M : 1 페치 조인
+            String query = "select distinct t from Team t join fetch t.members";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .getResultList();
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "+|members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
-
-
-
 
             tx.commit();
         } catch (Exception e) {
