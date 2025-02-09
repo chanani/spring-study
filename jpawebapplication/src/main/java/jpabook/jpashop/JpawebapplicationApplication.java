e2bc23c1ea555bf0505d3e18fdb8afca280b3256
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-09 15:42:38 +0900

    Re-added jpawebapplication after cache reset

---
+++ jpawebapplication/src/main/java/jpabook/jpashop/JpawebapplicationApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpawebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpawebapplicationApplication.class, args);
+	}
+
+}
