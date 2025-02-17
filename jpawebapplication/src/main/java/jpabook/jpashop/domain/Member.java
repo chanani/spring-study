66c29af4c8c05b80a202b6651379ae76b9a5a1dd
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-17 21:08:06 +0900

    [JPA WEB] 회원 조회  API 제작

--- jpawebapplication/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpawebapplication/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -22,6 +23,7 @@ public class Member {
     private Address address;
 
     // order 테이블에 있는 member 필드에 의해 매핑된 것임을 표시(읽기 전용)
+    // @JsonIgnore // 반환 시 컬럼 제거
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
