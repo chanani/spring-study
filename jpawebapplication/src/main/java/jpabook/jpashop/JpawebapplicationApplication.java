aab46d82ac668be2ee01c55f28fd753415986aef
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-02-18 23:19:44 +0900

    [JPA API] 주문 조회 성능 최적화(엔티티를 DTO로 변환)

--- jpawebapplication/src/main/java/jpabook/jpashop/JpawebapplicationApplication.java
+++ jpawebapplication/src/main/java/jpabook/jpashop/JpawebapplicationApplication.java
@@ -12,11 +12,11 @@ public class JpawebapplicationApplication {
         SpringApplication.run(JpawebapplicationApplication.class, args);
     }
 
-    @Bean
-    Hibernate5Module hibernate5Module() {
-        Hibernate5Module hibernate5Module = new Hibernate5Module();
-        //hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
-        return hibernate5Module;
-    }
+//    @Bean
+//    Hibernate5Module hibernate5Module() {
+//        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        //hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+//        return hibernate5Module;
+//    }
 
 }
