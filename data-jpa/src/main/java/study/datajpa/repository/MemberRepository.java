82c75646108aea95939c6a7e4d10c8515d0bd21f
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-03-22 16:19:09 +0900

    [DATA-JPA] NamedQuery

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -8,5 +10,11 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // 관례상 NamedQuery를 우선적으로 찾아서 진행 그래서 주석해도 정상적으로 작동됨
+    // 실무에서 잘 사용 안함
+    // @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
 
 }
