39b1add01e53163d9547c5b82071eca722cbc9d2
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-06 22:12:06 +0900

    [Query Dsl] 사용자 정의 리포지토리

--- querydsl/src/test/java/com/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/com/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.study.querydsl.repository;
 
+import com.study.querydsl.dto.MemberSearchCondition;
+import com.study.querydsl.dto.MemberTeamDto;
 import com.study.querydsl.entity.Member;
+import com.study.querydsl.entity.Team;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +37,35 @@ class MemberRepositoryTest {
 
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        em.flush();
+        em.clear();
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> dto = memberRepository.search(condition);
+
+        assertThat(dto).extracting("username").containsExactly("member4");
     }
 
 }
