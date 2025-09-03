3334fd4b0047516af76ffc61c599991f6f76597c
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-09-03 21:00:46 +0900

    [REDIS] @Cacheable 설정

--- redis/src/main/java/com/study/redis/config/RedisCacheConfig.java
+++ redis/src/main/java/com/study/redis/config/RedisCacheConfig.java
@@ -32,6 +32,7 @@ public class RedisCacheConfig {
                 )
                 // 데이터의 만료기간(TTL) 설정
                 .entryTtl(Duration.ofMinutes(1L));
+
         return RedisCacheManager
                 .RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory)
