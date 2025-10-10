3c18bb21c31b98e8aa8e1e9c58a2decf5b5a738c
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-10-10 16:44:01 +0900

    [FILTER, INTERCEPTOR] login argument resolver 생성

--- login/src/main/java/com/study/login/WebConfig.java
+++ login/src/main/java/com/study/login/WebConfig.java
@@ -1,5 +1,6 @@
 package com.study.login;
 
+import com.study.login.web.argumentresolver.LoginMemberArgumentResolver;
 import com.study.login.web.filter.LogFilter;
 import com.study.login.web.filter.LoginCheckFilter;
 import com.study.login.web.interceptor.LogInterceptor;
@@ -8,11 +9,19 @@ import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
