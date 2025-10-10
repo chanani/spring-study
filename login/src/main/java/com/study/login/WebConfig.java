8d38345d98e8f6abeba6f654e5428e0768ee8134
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-10-10 15:11:54 +0900

    [FILTER, INTERCEPTOR] log Interceptor 제작

--- login/src/main/java/com/study/login/WebConfig.java
+++ login/src/main/java/com/study/login/WebConfig.java
@@ -2,15 +2,30 @@ package com.study.login;
 
 import com.study.login.web.filter.LogFilter;
 import com.study.login.web.filter.LoginCheckFilter;
+import com.study.login.web.interceptor.LogInterceptor;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**",
+                        "/*.ico",
+                        "/error"
+                );
+    }
+
+    // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new LogFilter());
