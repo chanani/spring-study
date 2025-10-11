cdd23f9af555148de06d3f5ec5cff86c38a0ebf3
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-10-11 15:48:20 +0900

    [TYPE CONVERTER] httpServletRequest, @RequestParam으로 데이터 확인

---
+++ typeconverter/src/main/java/com/study/typeconverter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.study.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // getParameter의 반환 타입 String
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request){
+        String data = request.getParameter("data"); // 문자 타입 조회
+        Integer intValue = Integer.valueOf(data); // 숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV1(@RequestParam("data") Integer data){
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
