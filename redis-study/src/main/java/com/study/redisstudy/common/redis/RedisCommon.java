b203ff9bbb30bf4fb0d592997c14e6b2331d4521
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-10-25 18:19:20 +0900

    [REDIS] redis template sortedSet 함수 생성

--- redis-study/src/main/java/com/study/redisstudy/common/redis/RedisCommon.java
+++ redis-study/src/main/java/com/study/redisstudy/common/redis/RedisCommon.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -41,7 +43,7 @@ public class RedisCommon {
     }
 
     // 다중 데이터 저장
-    public <T> void multiSetData(Map<String, T> datas){
+    public <T> void multiSetData(Map<String, T> datas) {
         Map<String, String> jsonMap = new HashMap<>();
 
         for (Map.Entry<String, String> entry : jsonMap.entrySet()) {
@@ -49,6 +51,41 @@ public class RedisCommon {
         }
 
         template.opsForValue().multiSet(jsonMap);
+    }
+
+    // sortedSet 데이터 저장
+    public <T> void addSortedSet(String key, T value, Float score) {
+        String jsonValue = gson.toJson(value);
+        template.opsForZSet().add(key, jsonValue, score);
+    }
+
+    // score 기반으로 데이터 조회
+    public <T> Set<T> rangeByScore(String key, Float minScore, Float maxScore, Class<T> clazz) {
+        Set<String> jsonValues = template.opsForZSet().rangeByScore(key, minScore, maxScore);
+        Set<T> resultSet = new HashSet<>();
+
+        if (jsonValues != null) {
+            for (String jsonValue : jsonValues) {
+                T v = gson.fromJson(jsonValue, clazz);
+                resultSet.add(v);
+            }
+        }
+
+        return resultSet;
+    }
+
+    // sortedSet으로 랭킹을 구하기 위한 함수
+    public <T> Set<T> getTopNFromSortedSet(String key, int n, Class<T> clazz) {
+        Set<String> jsonValues = template.opsForZSet().range(key, 0, n - 1);
+        Set<T> resultSet = new HashSet<>();
+        if (jsonValues != null) {
+            for (String jsonValue : jsonValues) {
+                T v = gson.fromJson(jsonValue, clazz);
+                resultSet.add(v);
+            }
+        }
+
+        return resultSet;
     }
 
 }
